0d27bcb02e245f165427bb9882681ac7f14f61d0
Yvonne Biegon <biegonchela@gmail.com>
2025-02-11 20:21:33 +0300

    Add files via upload

---
+++ area of triangle(sample).cpp
@@ -0,0 +1,13 @@
+//area of rectangle
+#include <iostream>
+using namespace std;
+int main() {
+    int length=3;
+    int width=4;
+    int area=length*width;
+    cout << "the length of the rectangle is " << length << "\n";
+    cout << "the width of the rectangle is " << width << "\n";
+    cout << "the area of the rectangle is " << area << "\n";
+    
+    return 0;
+}
