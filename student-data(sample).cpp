6c56fbd800bc904c143c96056709cef4c9ced18d
Yvonne Biegon <biegonchela@gmail.com>
2025-02-11 20:22:17 +0300

    Add files via upload

---
+++ student-data(sample).cpp
@@ -0,0 +1,15 @@
+//student data
+#include <iostream>
+using namespace std;
+
+int main() {
+    int student_age=19;
+    int student_fee=224000;
+    char student_grade='A';
+    
+    cout <<"student_age:"<<student_age<< "\n";
+    cout <<"student_fee:"<<student_fee<< "\n";
+    cout <<"student_grade:"<<student_grade<< "\n";
+
+    return 0 ;
+}
